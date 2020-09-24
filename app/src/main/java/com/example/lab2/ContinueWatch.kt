04c8be0235702e5af726c1012cee2db238fb174e
Alexey <Bedrin18@mail.ru>
2020-09-24 16:23:00 +0300

    task4 completed

--- app/src/main/java/com/example/lab2/ContinueWatch.kt
+++ app/src/main/java/com/example/lab2/ContinueWatch.kt
@@ -1,36 +1,58 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.continuewatch.*
 
 class ContinueWatch : AppCompatActivity() {
     var secondsElapsed: Int = 0
-
+    var bgThreadActive: Boolean = true
     var backgroundThread = Thread {
         while (true) {
-            Thread.sleep(1000)
+            if (bgThreadActive) secondsElapsed++
             textSecondsElapsed.post {
-                textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
+                textSecondsElapsed.text = String.format("Seconds elapsed: %s", secondsElapsed)
             }
+            Thread.sleep(1000)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        if (savedInstanceState != null) {
-            with(savedInstanceState) { secondsElapsed = getInt(SECONDS) }
-        }
-
         setContentView(R.layout.continuewatch)
-        textSecondsElapsed.text = "Seconds elapsed: " + secondsElapsed++
+        bgThreadActive = true
         backgroundThread.start()
     }
 
+    override fun onPause() {
+        super.onPause()
+        bgThreadActive = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bgThreadActive = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        bgThreadActive = false
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        bgThreadActive = true
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.run { putInt(SECONDS, secondsElapsed) }
         super.onSaveInstanceState(outState)
+        outState.putInt(SECONDS, secondsElapsed)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        secondsElapsed = savedInstanceState.getInt(SECONDS)
     }
 
     companion object { const val SECONDS = "secondsElapsed" }
